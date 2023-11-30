91209b66dc72d2dc0e6b7f0ee825471aba03b8f9
Omarchg <147033400+Omarchg@users.noreply.github.com>
2023-11-30 20:57:22 +0000

    options

--- Assets/sub_menus.cs
+++ Assets/sub_menus.cs
@@ -21,52 +21,53 @@ public class sub_menus : MonoBehaviour
         status = false;
 
         bttn_1_1 = GameObject.Find("Arbol1").GetComponent<Button>();
-        bttn_1_2 = GameObject.Find("bttn_1_2").GetComponent<Button>();
-        bttn_1_3 = GameObject.Find("bttn_1_3").GetComponent<Button>();
-        bttn_1_4 = GameObject.Find("bttn_1_4").GetComponent<Button>();
+        bttn_1_2 = GameObject.Find("Arbol2").GetComponent<Button>();
+        bttn_1_3 = GameObject.Find("Arbol3").GetComponent<Button>();
+        bttn_1_4 = GameObject.Find("Arbol4").GetComponent<Button>();
 
-        bttn_2_1 = GameObject.Find("bttn_2_1").GetComponent<Button>();
-        bttn_2_2 = GameObject.Find("bttn_2_2").GetComponent<Button>();
-        bttn_2_3 = GameObject.Find("bttn_2_3").GetComponent<Button>();
-        bttn_2_4 = GameObject.Find("bttn_2_4").GetComponent<Button>();
+        bttn_2_1 = GameObject.Find("Arbusto1").GetComponent<Button>();
+        bttn_2_2 = GameObject.Find("Arbusto2").GetComponent<Button>();
+        bttn_2_3 = GameObject.Find("Arbusto3").GetComponent<Button>();
+        bttn_2_4 = GameObject.Find("Arbusto4").GetComponent<Button>();
 
-        bttn_3_1 = GameObject.Find("bttn_3_1").GetComponent<Button>();
-        bttn_3_2 = GameObject.Find("bttn_3_2").GetComponent<Button>();
-        bttn_3_3 = GameObject.Find("bttn_3_3").GetComponent<Button>();
-        bttn_3_4 = GameObject.Find("bttn_3_4").GetComponent<Button>();
+        bttn_3_1 = GameObject.Find("Elemento1").GetComponent<Button>();
+        bttn_3_2 = GameObject.Find("Elemento2").GetComponent<Button>();
+        bttn_3_3 = GameObject.Find("Elemento3").GetComponent<Button>();
+        bttn_3_4 = GameObject.Find("Elemento4").GetComponent<Button>();
 
         bttn_1_1.onClick.AddListener(() => select_go(GameObject.Find("SM_Env_Tree_Apricot_Grown_01")));
-        bttn_1_2.onClick.AddListener(() => select_go(GameObject.Find("prefab_1_2_name")));
-        bttn_1_3.onClick.AddListener(() => select_go(GameObject.Find("prefab_1_3_name")));
-        bttn_1_4.onClick.AddListener(() => select_go(GameObject.Find("prefab_1_4_name")));
+        bttn_1_2.onClick.AddListener(() => select_go(GameObject.Find("SM_Generic_Tree_Patch_01 (17)")));
+        bttn_1_3.onClick.AddListener(() => select_go(GameObject.Find("SM_Env_Tree_Cherry_01 (27)")));
+        bttn_1_4.onClick.AddListener(() => select_go(GameObject.Find("SM_Env_Tree_Large_01 (2)")));
 
-        bttn_2_1.onClick.AddListener(() => select_go(GameObject.Find("prefab_2_1_name")));
-        bttn_2_2.onClick.AddListener(() => select_go(GameObject.Find("prefab_2_2_name")));
-        bttn_2_3.onClick.AddListener(() => select_go(GameObject.Find("prefab_2_3_name")));
-        bttn_2_4.onClick.AddListener(() => select_go(GameObject.Find("prefab_2_4_name")));
+        bttn_2_1.onClick.AddListener(() => select_go(GameObject.Find("SM_Env_Flowers_02 (67)")));
+        bttn_2_2.onClick.AddListener(() => select_go(GameObject.Find("SM_Prop_Plant_Wheat_Optimised_04")));
+        bttn_2_3.onClick.AddListener(() => select_go(GameObject.Find("SM_Generic_Grass_Patch_03 (124)")));
+        bttn_2_4.onClick.AddListener(() => select_go(GameObject.Find("SM_Prop_Plant_Ground_03_L2")));
       
-        bttn_3_1.onClick.AddListener(() => select_go(GameObject.Find("prefab_3_1_name")));
-        bttn_3_2.onClick.AddListener(() => select_go(GameObject.Find("prefab_3_2_name")));
-        bttn_3_3.onClick.AddListener(() => select_go(GameObject.Find("prefab_3_3_name")));
-        bttn_3_4.onClick.AddListener(() => select_go(GameObject.Find("prefab_3_4_name")));
+        bttn_3_1.onClick.AddListener(() => select_go(GameObject.Find("SM_Veh_Pickup_01 (5)")));
+        bttn_3_2.onClick.AddListener(() => select_go(GameObject.Find("SM_Veh_TractorOld_01 (3)")));
+        bttn_3_3.onClick.AddListener(() => select_go(GameObject.Find("SM_Prop_Tyre_01")));
+        bttn_3_4.onClick.AddListener(() => select_go(GameObject.Find("SM_Prop_Truck_Rusted_01 (1)")));
 
     } // end of Start() 
 
 
     // Update is called once per frame
+    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && status)
+        if (status)
         {
-
             Camera cmr = GameObject.Find("Camera").GetComponent<Camera>();
             Vector3 mouse_pstn = cmr.ViewportToWorldPoint(new Vector3(Input.mousePosition.x / Screen.width, 0f, 6f));
             Vector3 objt_pstn = new Vector3(mouse_pstn.x, 0f, mouse_pstn.z);
-            Instantiate(bttn_1_1,GameObject.Find("Camera").transform.position + new Vector3(0f,-5f,-15f), Quaternion.identity);
-
+            Instantiate(go, GameObject.Find("Camera").transform.position + new Vector3(0f, 0f, -15f), Quaternion.identity);
+            status = false; // Establecer status en false después de instanciar el objeto
         }
+    }
 
-    } // end of Update()                                                
+                  
 
 
 
